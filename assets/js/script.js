e339696b7254913b3d7ee32dbbab1c8bd9e3a54f
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-05-12 19:31:44 -0400

    5.5.8

--- assets/js/script.js
+++ assets/js/script.js
@@ -169,7 +169,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -206,16 +206,18 @@ $('.card .list-group').sortable({
   tolerance: 'pointer',
   helper: 'clone',
   activate: function(event) {
-    console.log('activate', this);
+    $(this).addClass('dropover');
+    $(".bottom-trash").addClass('bottom-trash-drag');
   },
   deactivate: function(event) {
-    console.log('deactivate', this);
+    $(this).removeClass('dropover');
+    $(".bottom-trash").removeClass('bottom-trash-drag');
   },
   over: function(event) {
-    console.log('over', event.target);
+    $(event.target).addClass('dropover-active');
   },
   out: function(event) {
-    console.log('out', event.target);
+    $(event.target).removeClass('dropover-active');
   },
   update: function(event) {
     var tempArr = [];
@@ -260,10 +262,10 @@ $('#trash').droppable({
     ui.draggable.remove();
   },
   over: function(event, ui) {
-    console.log('over');
+    $(".bottom-trash").addClass('bottom-trash-active');
   },
   out: function(event, ui) {
-    console.log('out');
+    $(".bottom-trash").removeClass('bottom-trash-active');
   }
 })
 
@@ -290,6 +292,13 @@ var auditTask = function(taskEl) {
     $(taskEl).addClass('list-group-item-warning');
   }
 };
+
+
+setInterval(function() {
+  $('.card .list-group-item').each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
 
 // load tasks for the first time
 loadTasks();
